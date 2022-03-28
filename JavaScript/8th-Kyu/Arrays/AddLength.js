b8748052ee7241536f823babe891316599da7803
EricQyhan <Ericqyhan@gmail.com>
2022-03-27 21:52:25 -0400

    new JavaScript codewars

---
+++ JavaScript/8th-Kyu/Arrays/AddLength.js
@@ -0,0 +1,3 @@
+function addLength(str) {
+  //start-here
+}
---
+++ JavaScript/8th-Kyu/Arrays/FindDifferenceAgeBetweenOldestYoungest.js
@@ -0,0 +1,17 @@
+// Find the difference between oldest and youngest famly members
+
+// function differenceInAges(ages) {
+//   let sortedAges = ages.sort((a, b) => a - b);
+//   let ageDifference = sortedAges[sortedAges.length - 1] - sortedAges[0];
+
+//   return [sortedAges[0], sortedAges[sortedAges.length - 1], ageDifference];
+// }
+
+function differenceInAges(ages) {
+  const min = Math.min(...ages);
+  const max = Math.max(...ages);
+  return [min, max, max - min];
+}
+
+console.log(differenceInAges([82, 15, 6, 38, 35]));
+console.log(differenceInAges([57, 99, 14, 32]));
