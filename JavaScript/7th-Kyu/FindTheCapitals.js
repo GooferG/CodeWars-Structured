2126fb814816eb10194efd9b392f1a9a4bd9d37d
EricQyhan <Ericqyhan@gmail.com>
2022-04-08 20:44:27 -0400

    added 7th kyu on Find The Capitals

--- JavaScript/7th-Kyu/FindTheCapitals.js
+++ JavaScript/7th-Kyu/FindTheCapitals.js
@@ -1,7 +1,7 @@
 let capitals = (word) => {
   // Write your code here
   let capitalizeLetters = [];
-  for (let i = 0; i <= word.length; i++) {
+  for (let i = 0; i < word.length; i++) {
     if (word[i].toUpperCase() == word[i]) capitalizeLetters.push(i);
   }
   return capitalizeLetters;
